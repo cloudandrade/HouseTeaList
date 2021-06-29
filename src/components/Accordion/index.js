1c95abbb7b3973cb79ad8f814500c73dee2239a6
Jan Cloude <jcandrade@stefanini.com>
2021-06-29 17:09:41 -0300

    fix: colocando loading

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -8,6 +8,7 @@ import {
 	withStyles,
 	TextField,
 	Button,
+	CircularProgress,
 } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import './styles.css';
@@ -26,6 +27,17 @@ const GreenCheckbox = withStyles({
 	checked: {},
 })((props) => <Checkbox color="default" {...props} />);
 //<<<<<<<<<<<<<<checkbox
+//>>>>>>>>>>>>>>loading
+const loadStyles = makeStyles((theme) => ({
+	root: {
+		display: 'flex',
+		'& > * + *': {
+			marginLeft: theme.spacing(2),
+		},
+	},
+}));
+//<<<<<<<<<<<<<<loading
+
 //>>>>>>>>>>>>>textfield
 const CssTextField = withStyles({
 	root: {
@@ -56,6 +68,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SimpleAccordion() {
 	const classes = useStyles();
+	const loadclasses = loadStyles();
 	const textRef = React.useRef();
 	const [lista, setLista] = useState([]);
 	const [nome, setNome] = useState('');
@@ -65,6 +78,8 @@ export default function SimpleAccordion() {
 	}, []);
 
 	function fetchData() {
+		setLista([]);
+
 		getAll()
 			.then((response) => {
 				setLista(response.data);
@@ -90,84 +105,103 @@ export default function SimpleAccordion() {
 
 	return (
 		<div className={classes.root}>
-			{lista.map(function (item, index) {
-				return (
-					<div>
-						<Accordion className="accordion" key={item.id}>
-							<AccordionSummary
-								expandIcon={<ExpandMoreIcon />}
-								aria-controls="panel1a-content"
-								id="panel1a-header"
-							>
-								<FormControlLabel
-									control={<GreenCheckbox checked={item.checked} />}
-								/>
-								<Typography style={{ marginTop: '5px', fontSize: 20 }}>
-									{item.item}
-								</Typography>
-							</AccordionSummary>
-							<AccordionDetails
-								style={
-									item.checked === false
-										? { backgroundColor: '#ffffff' }
-										: { backgroundColor: '#c38a9e' }
-								}
-							>
-								{/**Aqui é validado se o check é true ou não */
-								/** se o
-								check for false exibirá o campo para preencher o nome e
-								botao */
-								/** se o check for true exibira o nome de quem
-								assinou */}
-								{item.checked === false ? (
-									<Typography>
-										<form>
-											<CssTextField
-												className={classes.margin}
-												className="text"
-												label="Seu Nome"
-												variant="outlined"
-												size="small"
-												onBlur={(e) => setNome(e.target.value)}
-											/>
-
-											<Button
-												variant="contained"
-												className="button"
-												style={{
-													marginLeft: '10px',
-													marginTop: '4px',
-													backgroundColor: '#c38a9e',
-												}}
-												onClick={(e) => handleAssinar(item)}
-											>
-												Assinar
-											</Button>
-										</form>
+			{lista.length === 0 ? (
+				<div
+					style={{
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'center',
+					}}
+				>
+					<CircularProgress style={{ color: '#c38a9e' }} />
+				</div>
+			) : (
+				lista.map(function (item, index) {
+					return (
+						<div>
+							<Accordion className="accordion" key={item.id}>
+								<AccordionSummary
+									expandIcon={<ExpandMoreIcon />}
+									aria-controls="panel1a-content"
+									id="panel1a-header"
+								>
+									<FormControlLabel
+										control={<GreenCheckbox checked={item.checked} />}
+									/>
+									<Typography
+										style={{ marginTop: '5px', fontSize: 20 }}
+									>
+										{item.item}
 									</Typography>
-								) : (
-									<div className="assinado-div">
-										<Typography
-											style={{
-												fontSize: 16,
-												fontWeight: 'bold',
-												marginLeft: '10px',
-											}}
-										>
-											Assinado por:
+								</AccordionSummary>
+								<AccordionDetails
+									style={
+										item.checked === false
+											? { backgroundColor: '#ffffff' }
+											: { backgroundColor: '#c38a9e' }
+									}
+								>
+									{/**Aqui é validado se o check é true ou não */
+									/** se o
+									check for false exibirá o campo para preencher o nome e
+									botao */
+									/** se o check for true exibira o nome de quem
+									assinou */}
+									{item.checked === false ? (
+										<Typography>
+											<form>
+												<CssTextField
+													className={classes.margin}
+													className="text"
+													label="Seu Nome"
+													variant="outlined"
+													size="small"
+													onBlur={(e) =>
+														setNome(e.target.value)
+													}
+												/>
+
+												<Button
+													variant="contained"
+													className="button"
+													style={{
+														marginLeft: '10px',
+														marginTop: '4px',
+														backgroundColor: '#c38a9e',
+													}}
+													onClick={(e) => handleAssinar(item)}
+												>
+													Assinar
+												</Button>
+											</form>
 										</Typography>
-										<Typography
-											style={{ fontSize: 16, marginLeft: '5px' }}
-										>
-											{item.nome}
-										</Typography>
-									</div>
-								)}
-							</AccordionDetails>
-						</Accordion>
-					</div>
-				);
-			})}
+									) : (
+										<div className="assinado-div">
+											<Typography
+												style={{
+													fontSize: 16,
+													fontWeight: 'bold',
+													marginLeft: '10px',
+												}}
+											>
+												Assinado por:
+											</Typography>
+											<Typography
+												style={{
+													fontSize: 16,
+													marginLeft: '5px',
+												}}
+											>
+												{item.nome}
+											</Typography>
+										</div>
+									)}
+								</AccordionDetails>
+							</Accordion>
+						</div>
+					);
+				})
+			)}
 		</div>
 	);
 }
